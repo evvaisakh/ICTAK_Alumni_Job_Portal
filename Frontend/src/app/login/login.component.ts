888db79b32f59f87cd3d654f93a5885da10f683e
Vaisakh Vijayan <evvaisakh4@gmail.com>
2023-07-28 23:27:57 +0530

    updated

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+declare var $:any;
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  login:FormGroup|any;
+  constructor(private http:HttpClient, private route:Router) {}
 
   ngOnInit(): void {
-
+    this.login = new FormGroup({
+      'fname': new FormControl(),
+      'password': new FormControl()
+    })
   }
+  logindata(login:FormGroup){
+    // console.log(this.login.value);
+     this.http.get<any>("http://localhost:3000/signup")
+     .subscribe(res=>{
+       const user = res.find((a:any)=>{
+         return a.fname === this.login.value.fname && a.password === this.login.value.password
+       });
+ 
+       if(user){
+         alert('you are successfully login');
+         this.login.reset();
+         $('.form-box').css('display','none');
+         this.route.navigate(['dashboard']);
+       }else{
+         alert('User Not Found');
+         this.route.navigate(['login']);
+       }
+ 
+     }, err=>{
+       alert('Something was wrong');
+     })
+    
+ 
+   }
+ 
+   sbtn1(){
+     $('.form-box').css('display','none');
+     $('.form-box1').css('display','block');
+   }
 
 }
--- Frontend/src/app/signup/signup.component.ts
+++ Frontend/src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+declare var $:any;
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() {}
+  constructor(private route:Router, private http:HttpClient) {}
+  signup:FormGroup|any;
+  signuser:any;
 
   ngOnInit(): void {
-    
+    this.signup = new FormGroup({
+      'fname': new FormControl(),
+      'lname':new FormControl(),
+      'email':new FormControl(),
+      'phone':new FormControl(),
+      'password': new FormControl()
+    })
+  }
+  signupdata(singup:FormGroup){
+    //console.log(this.singup.value);
+    this.signuser = this.signup.value.fname
+    this.http.post<any>("http://localhost:3000/signup", this.signup.value)
+    .subscribe(res=>{
+      alert('data added successfully');
+      this.signup.reset();
+      this.route.navigate(['login']);
+    }, err=>{
+      alert('Somthing went wrong');
+    })
+
   }
 
+  sbtn(){
+   
+    this.route.navigate(['login']);
+    //$('.form-box1').css('z-index', '99');
+    $('.form-box').css('display','block');
+    $('.form-box1').css('display','none');
+  }
 }
